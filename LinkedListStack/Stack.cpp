aa0591f0a79b5d9e5bb791c9958a5016ea81a41b
CorpseLiquor <ervjbnkwebkhjwer@gmail.com>
2018-07-11 22:10:43 -0400

    Still a few memory leaks

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,34 @@
 #include "Stack.h"
+
+void Stack::Push( int val ) {
+	if ( count == 0 ) {
+		e = new Element( val, nullptr );
+	} else {
+		Element* temp = e;
+		e = new Element( val, temp );
+		temp->Next = e;
+	}
+	count++;
+}
+
+int Stack::Pop() {
+	if(count > 0 ) {
+		count--;
+		Element* temp = e;
+		const int retVal = e->Value;
+		e = e->Previous;
+		delete temp;
+		temp = nullptr;
+		return retVal;
+	} else {
+		return -1;
+	}
+}
+
+int Stack::Size() const {
+	return count;
+}
+
+bool Stack::Empty() const {
+	return (count == 0);
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -1,10 +1,66 @@
 #pragma once
 
-class Stack
-{
+class Stack {
 public:
 	void Push( int val );
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+
+	Stack() = default;
+
+	~Stack() {
+		delete e;
+		e = nullptr;
+	}
+
+	Stack( const Stack& s ) {
+		Element* temp = s.e;
+		while ( temp != nullptr && temp->Previous != nullptr ) {
+			temp = temp->Previous;
+		}
+
+		while ( temp != nullptr ) {
+			this->Push( temp->Value );
+			temp = temp->Next;
+		}
+	}
+
+	Stack& operator=( const Stack& s ) {
+		Element* temp = s.e;
+		while ( temp != nullptr  && temp->Previous != nullptr ) {
+			temp = temp->Previous;
+		}
+
+		while ( temp != nullptr ) {
+			this->Push( temp->Value );
+			temp = temp->Next;
+		}
+
+		return *this;
+	}
+
+private:
+	int count = 0;
+
+
+	class Element {
+	public:
+		int Value;
+		Element* Next = nullptr;
+		Element* Previous = nullptr;
+
+		Element( const int val, Element* prev )
+			:
+			Value( val ),
+			Previous( prev ) {
+		}
+
+		Element( const Element& e ) = delete;
+		Element& operator=( const Element& e ) = delete;
+
+		~Element() = default;
+	};
+
+	Element* e;
 };
